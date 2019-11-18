7cdd258a03ba29a46bfbdb8272d2713a8983f79c
florian <florian.dupont.lp4@gmail.com>
2019-11-18 17:10:30 +0100

    modif tablelink et script sql

--- src/main/java/jdbc/TableLink.java
+++ src/main/java/jdbc/TableLink.java
@@ -1,5 +1,50 @@
 package jdbc;
 
-public class TableLink {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class TableLink {
+	
+	Connection conn;
+	
+	public TableLink(Connection conn) {
+		this.conn = conn;
+	}
+
+	public void addLink(String effect ,int idPlant , String typeEffect ) throws SQLException {
+		String tableEffect;	
+		switch (typeEffect) {
+		   case "medical" : 
+			  tableEffect = "meffect";
+				break;
+		   case "positive" : 
+			 tableEffect = "peffect";
+			    break;
+			default :   
+			 tableEffect = "neffect";
+				break;
+		}
+		
+		PreparedStatement stmt = this.conn.prepareStatement("select id_"+ tableEffect +" from "+ tableEffect +" where name_"+ tableEffect +" = ?");
+		
+		stmt.setString(1, effect);
+		System.out.println(stmt);
+		ResultSet rs = stmt.executeQuery();
+		
+		if (rs.next()) {
+		int id = rs.getInt("id_"+ tableEffect);
+		System.out.println(id);
+		
+		}
+		
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		ConnectTable test = new ConnectTable();
+		TableLink test2 = new TableLink(test.getConnection());
+		test2.addLink("Stress", 1, "medical");
+	}
+	
 }
